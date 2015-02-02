e1d2cdfb29b96d547042ecd71a60afd0f2264b29
mcpool <tkalantzis@gr-linux.net>
2015-02-02 17:47:19 +0200

    Digibyte SHA256 network file

---
+++ p2pool/networks/digibyteSha.py
@@ -0,0 +1,18 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['digibyteSha']
+SHARE_PERIOD = 30 # seconds target spacing
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 20 # shares diff regulation
+SPREAD = 10 # blocks
+IDENTIFIER = '48a4ebc31b798115'.decode('hex')
+PREFIX = '5685a276c2dd81db'.decode('hex')
+P2P_PORT = 5010
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**32 - 1
+PERSIST = False
+WORKER_PORT = 5011
+BOOTSTRAP_ADDRS = 'dgbsha.mastercryptopool.net dgbsha2.mastercryptopool.net'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-alt'
+VERSION_CHECK = lambda v: True
