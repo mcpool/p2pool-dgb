754c4b8e77f1f51c8ddc15682ddd67160059b417
mcpool <tkalantzis@gr-linux.net>
2015-02-07 14:33:30 +0200

    Update digibyteSha.py

--- p2pool/bitcoin/networks/digibyteSha.py
+++ p2pool/bitcoin/networks/digibyteSha.py
@@ -19,10 +19,10 @@ SUBSIDY_FUNC = lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(h
 POW_FUNC=data.hash256
 BLOCK_PERIOD = 30 # s
 SYMBOL = 'DGB'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf')
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibytesha'), 'digibyte.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://digiexplorer.info/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://digiexplorer.info/address/'
 TX_EXPLORER_URL_PREFIX = 'http://digiexplorer.info/tx/'
 SANE_TARGET_RANGE=(2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
-DUMB_SCRYPT_DIFF = 2
+DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
