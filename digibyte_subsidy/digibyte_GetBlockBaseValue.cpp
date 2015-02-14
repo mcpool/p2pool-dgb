7640af2e101e49f7c752131b9282f33a8f21ace9
mcpool <tkalantzis@gr-linux.net>
2015-02-14 03:38:31 +0200

    Update digibyte_GetBlockBaseValue.cpp

--- digibyte_subsidy/digibyte_GetBlockBaseValue.cpp
+++ digibyte_subsidy/digibyte_GetBlockBaseValue.cpp
@@ -2,28 +2,42 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
-#include <math.h> 
+#include <math.h>
 
 static const int64_t COIN = 100000000;
 
 static const int64_t nDiffChangeTarget = 67200; // Patch effective @ block 67200
 static const int64_t patchBlockRewardDuration = 10080; // 10080 blocks main net change
+static const int64_t patchBlockRewardDuration2 = 80160; // 80160 blocks main net change
+static const int64_t patchBlockRewardDuration3 = 400000; // block 400000 after which all difficulties are updated on every block
 
 int64_t GetDGBSubsidy(int nHeight) {
-   // thanks to RealSolid & WDC for helping out with this code
-   int64_t qSubsidy = 8000*COIN;
-   int blocks = nHeight - nDiffChangeTarget;
-   int weeks = (blocks / patchBlockRewardDuration)+1;
-   //decrease reward by 0.5% every week
-   for(int i = 0; i < weeks; i++)  qSubsidy -= (qSubsidy/200);  
-   return qSubsidy;
+        int64_t qSubsidy;
 
+        if (nHeight < patchBlockRewardDuration3)
+        {
+                int64_t qSubsidy = 8000*COIN;
+                int blocks = nHeight - nDiffChangeTarget;
+                int weeks = (blocks / patchBlockRewardDuration)+1;
+                //decrease reward by 0.5% every 10080 blocks
+                for(int i = 0; i < weeks; i++)  qSubsidy -= (qSubsidy/200);
+        }
+        else
+        else
+        {
+                int64_t qSubsidy = 2459*COIN;
+                int blocks = nHeight - patchBlockRewardDuration3;
+                int weeks = (blocks / patchBlockRewardDuration2)+1;
+                //decrease reward by 1% every month
+                for(int i = 0; i < weeks; i++)  qSubsidy -= (qSubsidy/100);
+        }
+        return qSubsidy;
 }
 
 int64_t static  GetBlockBaseValue(int nHeight) {
 
    int64_t nSubsidy = COIN;
-   
+
    if(nHeight < nDiffChangeTarget) {
       //this is pre-patch, reward is 8000.
       nSubsidy = 8000 * COIN;
@@ -35,7 +49,7 @@ int64_t static  GetBlockBaseValue(int nHeight) {
       {
         nSubsidy = 16000 * COIN;
       }
-      
+
    } else {
       //patch takes effect after 67,200 blocks solved
       nSubsidy = GetDGBSubsidy(nHeight);
@@ -53,7 +67,7 @@ int64_t static  GetBlockBaseValue(int nHeight) {
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
 using namespace boost::python;
- 
+
 BOOST_PYTHON_MODULE(digibyte_subsidy)
 {
     def("GetBlockBaseValue", GetBlockBaseValue);
